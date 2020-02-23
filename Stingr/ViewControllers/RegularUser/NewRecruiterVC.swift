e725584b7e7dde93be33eaa8470777a1be428330
Stephon Fonrose <stephonfonrose@gmail.com>
2020-02-23 12:15:22 -0500

    thinga

--- Stingr/ViewControllers/RegularUser/NewRecruiterVC.swift
+++ Stingr/ViewControllers/RegularUser/NewRecruiterVC.swift
@@ -22,7 +22,7 @@ class NewRecruiterVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        finishRegistration()
     }
     
     func finishRegistration() {
